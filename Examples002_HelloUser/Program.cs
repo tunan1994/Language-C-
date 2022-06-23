048503a7fb71dc0de0194eacf33517991de79471
tunan1994 <tunan@mail.ru>
2022-06-23 18:55:04 +0300

    first commit

---
+++ Examples002_HelloUser/Program.cs
@@ -0,0 +1,10 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.Write("ВВедите своё имя, ");
+        string usernama = Console.ReadLine();
+        Console.Write("Hi, ");
+        Console.Write(username);
+    }
+}
