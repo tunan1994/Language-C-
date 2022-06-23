7804020bbf2b5482485f05a68dc15c87bdf40791
tunan1994 <tunan@mail.ru>
2022-06-23 23:34:05 +0300

    list_max

---
+++ Example006_Base/Program.cs
@@ -0,0 +1,15 @@
+﻿int a = 1;
+int b = 2;
+int c = 3;
+int d = 4;
+int e = 5;
+
+int max = a;
+if (a > max) max = a;
+if (b > max) max =b;
+if (c > max) max = c;
+if (d > max) max =d;
+if (e > max) max =e;
+
+Console.Write("max = ");
+Console.WriteLine(max);
